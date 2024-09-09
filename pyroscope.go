17f89bef028801f6ba77ae67a3312d3b4cc3f89e
hakastein <headcrabogon@gmail.com>
2024-09-09 14:08:28 +0500

    exteneded logging refactor

--- pyroscope.go
+++ pyroscope.go
@@ -3,9 +3,9 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"net/http"
-
 	"go.uber.org/zap"
+	"net/http"
+	"net/http/httputil"
 )
 
 func combineTags(staticTags, dynamicTags string) string {
@@ -28,54 +28,72 @@ func sendToPyroscope(
 ) {
 	client := &http.Client{}
 
-	for {
-		val, ok := <-channel
-		if !ok {
-			break
+	val, ok := <-channel
+	if !ok {
+		return
+	}
+
+	var buffer bytes.Buffer
+
+	val.RLock()
+	defer val.RUnlock()
+
+	for dynamicTags, tagSamples := range val.samples {
+
+		fullTags := combineTags(staticTags, dynamicTags)
+
+		for _, sample := range tagSamples {
+			line := fmt.Sprintf("%s %d\n", sample.sample, sample.count)
+			buffer.WriteString(line)
 		}
 
-		val.RLock()
-		defer val.RUnlock()
+		buffLen := buffer.Len()
 
-		var buffer bytes.Buffer
-		for dynamicTags, tagSamples := range val.samples {
+		req, err := http.NewRequest("POST", pyroscopeURL+"/ingest", &buffer)
+		if err != nil {
+			logger.Error("Error creating request", zap.Error(err))
+			continue
+		}
 
-			fullTags := combineTags(staticTags, dynamicTags)
+		req.Header.Set("Content-Type", "text/plain")
+		if pyroscopeAuth != "" {
+			req.Header.Set("Authorization", pyroscopeAuth)
+		}
 
-			for _, sample := range tagSamples {
-				line := fmt.Sprintf("%s %d\n", sample.sample, sample.count)
-				buffer.WriteString(line)
-			}
+		q := req.URL.Query()
+		q.Add("name", fmt.Sprintf("%s{%s}", app, fullTags))
+		q.Add("from", fmt.Sprintf("%d", val.from.Unix()))
+		q.Add("until", fmt.Sprintf("%d", val.to.Unix()))
+		q.Add("sampleRate", fmt.Sprintf("%d", val.rateHz))
+		q.Add("format", "folded")
+		req.URL.RawQuery = q.Encode()
 
-			req, err := http.NewRequest("POST", pyroscopeURL+"/ingest", &buffer)
-			if err != nil {
-				logger.Error("Error creating request", zap.Error(err))
-				continue
-			}
+		resp, err := client.Do(req)
+		if err != nil {
+			logger.Error("Error sending request", zap.Error(err))
+			continue
+		}
 
-			req.Header.Set("Content-Type", "text/plain")
-			if pyroscopeAuth != "" {
-				req.Header.Set("Authorization", pyroscopeAuth)
-			}
+		if resp.Body.Close() != nil {
+			logger.Fatal("Got error while closing response body", zap.Error(err))
+		}
 
-			q := req.URL.Query()
-			q.Add("name", fmt.Sprintf("%s{%s}", app, fullTags))
-			q.Add("from", fmt.Sprintf("%d", val.from.Unix()))
-			q.Add("until", fmt.Sprintf("%d", val.to.Unix()))
-			q.Add("sampleRate", fmt.Sprintf("%d", val.rateHz))
-			q.Add("format", "folded")
-			req.URL.RawQuery = q.Encode()
+		logger.Info("samples sent",
+			zap.String("tags", fullTags),
+			zap.Int("bytes", buffLen),
+		)
 
-			resp, err := client.Do(req)
-			if err != nil {
-				logger.Error("Error sending request", zap.Error(err))
-				continue
-			}
-			defer resp.Body.Close()
+		if logger.Level() == zap.DebugLevel {
+			reqDump, _ := httputil.DumpRequest(req, true)
+			resDump, _ := httputil.DumpResponse(resp, true)
+			logger.Debug("samples query",
+				zap.String("request", string(reqDump)),
+				zap.String("response", string(resDump)),
+			)
+		}
 
-			if resp.StatusCode != http.StatusOK {
-				logger.Warn("Received non-OK response", zap.String("status", resp.Status))
-			}
+		if resp.StatusCode != http.StatusOK {
+			logger.Warn("Received non-OK response", zap.String("status", resp.Status))
 		}
 	}
 }
