5a7cf4260ec04aa58869fc12abbc87d82a507610
hakastein <headcrabogon@gmail.com>
2024-12-01 01:04:17 +0500

    Static tags are now validated on application startup

--- internal/tag/tag.go
+++ internal/tag/tag.go
@@ -5,9 +5,9 @@ import (
 	"strings"
 )
 
-// IsTagRuneAllowed checks if a rune is allowed in tag keys and values,
+// IsTagKeyRuneAllowed checks if a rune is allowed in tag keys and values,
 // this function is copy of IsTagKeyRuneAllowed from pyroscope/pkg/og/flameql/flameql.go
-func IsTagRuneAllowed(r rune) bool {
+func IsTagKeyRuneAllowed(r rune) bool {
 	return (r >= 'a' && r <= 'z') ||
 		(r >= 'A' && r <= 'Z') ||
 		(r >= '0' && r <= '9') ||
@@ -23,21 +23,21 @@ func ParseInput(tagsInput []string) (string, map[string]string, error) {
 	for _, tag := range tagsInput {
 		idx := strings.Index(tag, "=")
 		if idx == -1 {
-			return "", nil, fmt.Errorf("unexpected tag format %s, expected tag=value", tag)
+			return "", nil, fmt.Errorf("unexpected tag value `%s`, expected format is tag=value", tag)
 		}
 
 		key := tag[:idx]
 		value := tag[idx+1:]
 
-		if !isValid(key) {
-			return "", nil, fmt.Errorf("invalid tag key %s", key)
+		if !isValidTagKey(key) {
+			return "", nil, fmt.Errorf("invalid tag key `%s`", key)
 		}
 
 		if len(value) > 2 && value[0] == '%' && value[len(value)-1] == '%' {
 			dynamicTags[value[1:len(value)-1]] = key
 		} else {
-			if !isValid(value) {
-				return "", nil, fmt.Errorf("invalid tag value %s", value)
+			if strings.ContainsRune(value, ',') {
+				return "", nil, fmt.Errorf("invalid tag value `%s`, can't use coma symbol", value)
 			}
 			staticTags = append(staticTags, tag)
 		}
@@ -46,9 +46,9 @@ func ParseInput(tagsInput []string) (string, map[string]string, error) {
 	return strings.Join(staticTags, ","), dynamicTags, nil
 }
 
-func isValid(s string) bool {
+func isValidTagKey(s string) bool {
 	for _, r := range s {
-		if !IsTagRuneAllowed(r) {
+		if !IsTagKeyRuneAllowed(r) {
 			return false
 		}
 	}
